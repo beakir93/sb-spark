a9f5f1e8db0b760c28e2b8443cd72bac982c2742
beakir93 <beakir93@gmail.com>
2020-06-02 00:26:16 +0300

    lab04b

--- lab04b/agg/agg.scala
+++ lab04b/agg/agg.scala
@@ -28,6 +28,7 @@ object agg {
     if (fs.exists(checkPointPth))
       fs.delete(checkPointPth, true)
 
+
     sparkSession
       .readStream
       .format("kafka")
@@ -42,17 +43,30 @@ object agg {
       .groupBy(window(col("datetime"), "60 minutes"))
       .agg(min("timestamp").as("start_ts")
         ,max("timestamp").as("end_ts")
-        //,min("datetime").as("start_dt")
-        //,max("datetime").as("end_dt")
+        ,min("datetime").as("start_dt")
+        ,max("datetime").as("end_dt")
         ,sum(when(col("event_type") === "buy", col("item_price"))).as("revenue")
-        ,approx_count_distinct("uid").as("visitors")
-        ,approx_count_distinct(when(col("event_type") === "buy", col("uid"))).as("purchases")
+        ,approx_count_distinct(when(col("uid").isNull, "aaaaaoaoaoaoaoao0").otherwise(col("uid"))).as("visitors")
+        ,sum(when(col("event_type") === "buy", 1).otherwise(0)).as("purchases")
         ,(sum(when(col("event_type") === "buy", col("item_price")))
-          / approx_count_distinct(when(col("event_type") === "buy", col("uid")))).as("aov")
+                    / sum(when(col("event_type") === "buy", 1).otherwise(0))).as("aov")
       )
-      //.select(to_json(col("start_ts"), col("end_ts"), col("revenue"), col("visitors"), col("purchases"), col("aov")).as("json"))
+      .withColumn("start_ts_tms", unix_timestamp(col("start_dt")))
+      .withColumn("start_ts_1h_tms", unix_timestamp(col("start_dt") + expr("INTERVAL 1 HOURS")))
+      .select(col("start_ts_tms").as("start_ts")
+              ,col("start_ts_1h_tms").as("end_ts")
+              //,col("start_dt")
+              //,col("end_dt")
+              ,col("revenue")
+              ,col("visitors")
+              ,col("purchases")
+              ,col("aov")
+      )
+      .toJSON
       .writeStream
-      //.format("console")
+      /*.format("console")
+      .option("numRows", 1000)
+      .option("truncate", false)*/
       .format("kafka")
       .option("kafka.bootstrap.servers", "10.0.1.13:6667")
       .option("subscribe", "kirill_likhouzov_lab04b_out")
@@ -60,6 +74,7 @@ object agg {
       .option("checkpointLocation", "/user/kirill.likhouzov/laba04b/checkpoint")
       .start
       .awaitTermination(100000)
+
 
     System.out.println("Writing view table".toUpperCase)
 
