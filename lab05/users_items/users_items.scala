39354442a9e430326694374ff928119142c7165d
beakir93 <beakir93@gmail.com>
2020-06-08 23:12:35 +0300

    lab05 fin

--- lab05/users_items/users_items.scala
+++ lab05/users_items/users_items.scala
@@ -34,15 +34,6 @@ object users_items {
     System.out.println(s"output_dtr: $output_dir")
     System.out.println(s"input_dir: $input_dir")
 
-    //    val fs = FileSystem.get(sc.hadoopConfiguration)
-    //    val outPutPath = new Path(s"$output_dir_prefix/")
-    //      if (fs.exists(outPutPath))
-    //          fs.delete(outPutPath, true)
-    //  System.out.println("Dropped table".toUpperCase)
-
-    //    val today = new SimpleDateFormat("yMMdd").format(Calendar.getInstance().getTime())
-    //    System.out.println("Today date: " + today)
-
     def union_df_diff_col (df1: DataFrame, df2: DataFrame) = {
 
       def col_null (df1:DataFrame, df2:DataFrame) = {
@@ -71,23 +62,23 @@ object users_items {
       .read
       .json(s"$input_dir/buy")
 
-    System.out.println("df_buy")
-    df_buy.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
+//    System.out.println("df_buy")
+//    df_buy.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
 
     val df_view= sparkSession
       .read
       .json(s"$input_dir/view")
 
-    System.out.println("df_view")
-    df_view.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
+//    System.out.println("df_view")
+//    df_view.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
 
 
     val df_buy_view= df_buy
       .union(df_view)
 
     System.out.println("df_buy_view")
-    df_buy_view.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
-    df_buy_view.count
+//    df_buy_view.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
+//    df_buy_view.count
 
     val dt_max_arr= df_buy_view
       .agg(max(col("date")))
@@ -105,8 +96,8 @@ object users_items {
       .count
       .repartition(1)
 
-    System.out.println("df_pvt")
-    df_pvt.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
+//    System.out.println("df_pvt")
+//    df_pvt.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
 
 
     if (update_mode.toInt == 1) {
@@ -118,13 +109,13 @@ object users_items {
         .read
         .parquet(s"$output_dir/20200429")
 
-      System.out.println("users_items_old")
-      users_items_old.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
-      //TODO: заменить хардкод даты пути на чтение папок из hdfs
+//      System.out.println("users_items_old")
+//      users_items_old.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
+//      TODO: заменить хардкод даты пути на чтение папок из hdfs
 
-      val df_union = union_df_diff_col(users_items_old, df_pvt)
-      System.out.println("df_union")
-      df_union.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
+        val df_union = union_df_diff_col(users_items_old, df_pvt)
+//      System.out.println("df_union")
+//      df_union.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
 
       df_union
         .na.fill(0)
@@ -139,8 +130,6 @@ object users_items {
         .mode("append")
         .parquet(s"$output_dir/20200429")
     }
-
-
 
     System.out.println("Writing view table".toUpperCase)
 
