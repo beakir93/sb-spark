1d33829545c9cb7182c0fc5d243e57e78bafbe8a
beakir93 <beakir93@gmail.com>
2020-06-22 19:24:56 +0300

    fixed

--- lab05/users_items/users_items.scala
+++ lab05/users_items/users_items.scala
@@ -9,33 +9,33 @@ object users_items {
 
     import java.text.SimpleDateFormat
     import java.util.{Calendar, Date}
-
+    
     import org.apache.spark.sql.DataFrame
     //Create a SparkContext to initialize Spark
     val conf = new SparkConf()
       .setAppName("lab05")
-
+    
     conf.set("spark.sql.session.timeZone", "UTC")
-
+    
     val sparkSession = SparkSession.builder()
       .config(conf=conf)
       .getOrCreate()
-
+    
     var sc = sparkSession.sparkContext
     sc.setLogLevel("WARN")
-
+    
     println("SparkContext started".toUpperCase)
-
+    
     val update_mode = sc.getConf.get("spark.users_items.update")
     val output_dir = sc.getConf.get("spark.users_items.output_dir")
     val input_dir = sc.getConf.get("spark.users_items.input_dir")
-
+    
     println(s"update_mode: " + update_mode)
     System.out.println(s"output_dtr: $output_dir")
     System.out.println(s"input_dir: $input_dir")
-
+    
     def union_df_diff_col (df1: DataFrame, df2: DataFrame) = {
-
+    
       def col_null (df1:DataFrame, df2:DataFrame) = {
         var df_tmp :DataFrame = df2
         for (col <- df1.columns) {
@@ -45,18 +45,18 @@ object users_items {
         }
         df_tmp
       }
-
+    
       val df_1_tmp = col_null(df1, df2)
       val df_2_tmp = col_null(df2, df1)
-
+    
       val df_res = df_1_tmp.unionByName(df_2_tmp)
       df_res
     }
-
+    
     /****************************************************************************************/
     /*                                     logics                                           */
     /****************************************************************************************/
-
+    
     val df_buy= sparkSession
       .read
       .json(s"$input_dir/buy")
@@ -74,7 +74,7 @@ object users_items {
 
     val df_buy_view= df_buy
       .union(df_view)
-
+    
     System.out.println("df_buy_view")
 //    df_buy_view.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
 //    df_buy_view.count
@@ -85,7 +85,7 @@ object users_items {
       .map(x => x(0))
     val dt_max = dt_max_arr(0)
     System.out.println("dt_max: " + dt_max)
-
+    
     val df_pvt= df_buy_view
       .withColumn("item_type", when(col("event_type") === "buy",
         concat(lit("buy_"), lower(regexp_replace(regexp_replace(col("item_id"), "-", "_"), " ", "_"))))
@@ -99,14 +99,17 @@ object users_items {
 //    df_pvt.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
 
 
+    val dt_usr= "hdfs dfs -ls /user/kirill.likhouzov/users-items/".!!
+    val dt_usr_max= dt_usr.substring(126,134)
+    
     if (update_mode.toInt == 1) {
       System.out.println("update_mode == 1")
       System.out.println("hdfs dfs -ls file:///data/home/labchecker2/checkers/logs/sb1laba05/kirill.likhouzov/".!!)
       System.out.println("hdfs dfs -ls file:///data/home/labchecker2/checkers/logs/sb1laba05/kirill.likhouzov/users-items".!!)
-
+    
       val users_items_old = sparkSession
         .read
-        .parquet(s"$output_dir/20200429")
+        .parquet(s"$output_dir/$dt_usr_max")
 
 //      System.out.println("users_items_old")
 //      users_items_old.filter(col("uid") === "8bb01460217f871cbe0ae8fa1ceac2cc").show(3, 1000, true)
@@ -127,11 +130,11 @@ object users_items {
         .na.fill(0)
         .write
         .mode("append")
-        .parquet(s"$output_dir/20200429")
+        .parquet(s"$output_dir/$dt_usr_max")
     }
-
+    
     System.out.println("Writing view table".toUpperCase)
-
+    
     sc.stop()
 
   }
